59267f23ec3aec6118396173b5d381dbd0e1005f
cariventuresglobal246 <cariventuresglobal246@gmail.com>
2025-12-05 23:44:32 -0400

    Create supabaseClient.js

---
+++ src/supabaseClient.js
@@ -0,0 +1,7 @@
+// src/supabaseClient.js
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://lgurtucciqvwgjaphdqp.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxndXJ0dWNjaXF2d2dqYXBoZHFwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjk2MzgzNTAsImV4cCI6MjA0NTIxNDM1MH0.I1ajlHp5b4pGL-NQzzvcVdznoiyIvps49Ws5GZHSXzk';
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
